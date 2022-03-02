6be30bb69baef53de832c8a7d81d44ab5c6f4140
TheMightyFid <themightyfid@gmail.com>
2022-03-02 10:23:19 +0000

    Clean unwanted lines

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// 2022 Faculty Technical Interview - Chris Cunningham
+
+#include <iosfwd>
+#include <iostream>
+
+using namespace std;
+
+int
+main(int argc, char* argv[])
+{
+  cout << "hello world" << endl;
+  
+  for (int i = 0; i < 500; i++) {
+    cout << "print #" << i << endl;
+  }
+
+  return 0;
+}
